4b3cc34a3e161047a73901916b22345c42d0ccb8
xseser <mierzejewskiigor5@gmail.com>
2023-03-03 03:13:18 +0100

    interface for new entities.

--- demo/src/main/java/com/example/demo/repository/CardRepository.java
+++ demo/src/main/java/com/example/demo/repository/CardRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.repository;
 
-public interface CardRepository {
+import com.example.demo.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardRepository extends JpaRepository <Card, Long> {
 }
--- demo/src/main/java/com/example/demo/repository/WordRepository.java
+++ demo/src/main/java/com/example/demo/repository/WordRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.repository;
 
-public interface WordRepository {
+import com.example.demo.entity.Word;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WordRepository extends JpaRepository<Word, Long> {
 }
