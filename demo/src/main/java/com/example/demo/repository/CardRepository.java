45376dfcb9a6f3cd92aa620329b500cbba60582d
xseser <mierzejewskiigor5@gmail.com>
2023-03-03 05:45:15 +0100

    repositories does not work, I will figure it out tomorrow. It's a commit to end of work.

--- demo/src/main/java/com/example/demo/repository/CardRepository.java
+++ demo/src/main/java/com/example/demo/repository/CardRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-public interface CardRepository extends JpaRepository <Card, Long> {
+public interface CardRepository extends JpaRepository<Card, Long> {
 }
